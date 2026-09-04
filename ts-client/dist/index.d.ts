03e964eeb49be5c5e9fffab53a5e057a861e0cd2
MonSp <qgh132555@qq.com>
2026-09-04 08:58:18 +0800

    chore: remove ts-client/dist/ from tracking, add to .gitignore

--- ts-client/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { AgentKernelClient } from './AgentKernelClient';
-export { loadMappings, getGameAbility, getCompanySkills, resetCache } from './SkillMappingLoader';
-export type { AgentProfile, AgentRole, CareerStage, CreateAgentParams, GameIdentity, KernelRequest, KernelResponse, PersonalityTraits, SkillMapping, SkillNode, } from './types';
-//# sourceMappingURL=index.d.ts.map
